e17cbbb50c4793c132541b0693170447da892638
carrot <96284645+carrotshniper21@users.noreply.github.com>
2022-11-24 18:12:54 -0600

    Update pip-cli.py

--- pip-cli.py
+++ pip-cli.py
@@ -1,43 +1,13 @@
-mport urllib, re, os, subprocess
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import urllib, re, os, subprocess
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ChromeOptions, Chrome
 
-URL = "https://vipstream.tv/home"
-urls = ('''https://vipstream.tv/genre/action
-https://vipstream.tv/genre/action-adventure
-https://vipstream.tv/genre/adventure
-https://vipstream.tv/genre/animation
-https://vipstream.tv/genre/biography
-https://vipstream.tv/genre/comedy
-https://vipstream.tv/genre/crime
-https://vipstream.tv/genre/documentary
-https://vipstream.tv/genre/drama
-https://vipstream.tv/genre/family
-https://vipstream.tv/genre/fantasy
-https://vipstream.tv/genre/history
-https://vipstream.tv/genre/horror
-https://vipstream.tv/genre/kids
-https://vipstream.tv/genre/music
-https://vipstream.tv/genre/mystery
-https://vipstream.tv/genre/news
-https://vipstream.tv/genre/reality
-https://vipstream.tv/genre/romance
-https://vipstream.tv/genre/sci-fi-fantasy
-https://vipstream.tv/genre/science-fiction
-https://vipstream.tv/genre/soap
-https://vipstream.tv/genre/talk
-https://vipstream.tv/genre/thriller
-https://vipstream.tv/genre/tv-movie
-https://vipstream.tv/genre/war
-https://vipstream.tv/genre/war-politics
-https://vipstream.tv/genre/western
-''')
-
-genres = urls.replace("https://vipstream.tv/genre","")
-
-os.system('''
+def display_message():
+    os.system('''
 COLOR='\033[1;31m'
 NC='\033[0m' # No Color
 cat << EOF
@@ -46,92 +16,145 @@ ${COLOR}
   / /_/ // // /_/ /_____/ /   / /    / /
  / ____// // ____/_____/ /___/ /____/ /
 /_/   /___/_/          \____/_____/___/
-
-     Made by eat my nuts#4595
-       Credits to 4ce#6574
+       Made by eat my nuts#4595
+          Credits to 4ce#6574
 ${NC}
 EOF''')
-
-def SetupFirefox():
-        options = webdriver.FirefoxOptions()
-        options.headless = True
-
-        driver = webdriver.Firefox(options=options)
-        driver.get(URL)
-        return driver
+display_message()
 
 
-def GetGenres(driver):
-        elems = driver.find_elements(By.XPATH, "//a[@href]")
-        links = []
-        for elem in elems:
-                link = elem.get_attribute('href')
-                if 'genre' in elem.get_attribute('href'):
-                        links.append(link)
-        return links
+BASE_URL = "https://vipstream.tv/home"
 
-# Create an object to hold list of genres and links
-
-# 2d array of movie titles and links
-
-# Finding the download link
+#genres = genres.replace("https://vipstream.tv/genre/", "")
 
 
+def setup_firefox():
+    options = webdriver.FirefoxOptions()
+    options.headless = True
+    driver_f = webdriver.Firefox(options=options)
+    driver_f.get(BASE_URL)
+    return driver_f
 
 
-def GetUrlSource(URL, driver):
-    driver.get(URL)
-    page_source = driver.page_source
-    driver.close()
+def setup_chrome():
+    opts = ChromeOptions()
+    opts.headless = True
+    chrome_headless = webdriver.Chrome(options=opts)
+    return chrome_headless
+
+
+def get_genres(web_driver):
+    elems = web_driver.find_elements(By.XPATH, "//a[@href]")
+    links = []
+    for elem in elems:
+        link = elem.get_attribute('href')
+        if 'genre' in elem.get_attribute('href'):
+            links.append(link)
+    return links
+
+
+# ALMOST DONE Create an object to hold list of genres and links
+# TODO 2d array of movie titles and links
+# TODO Finding the download link
+
+def get_url_source(BASE_URL, browser_headless):
+    browser_headless.get(BASE_URL)
+    page_source = browser_headless.page_source
+    browser_headless.close()
     return page_source
 
 
 def write_to_file(page_source):
-        with open('page_source.html', 'w') as f:
-                f.write(page_source)
+    with open('page_source.html', 'w') as f:
+        f.write(page_source)
+
 
 def internet_working():
-        try:
-                urllib.request.urlopen('https://google.com', timeout=1)
-                return True
-        except urllib.request.URLError as err:
-                return False
+    try:
+        urllib.request.urlopen('https://google.com', timeout=1)
+        return True
+    except urllib.request.URLError as err:
+        return False
+
+
+def display_genres_to_user(genre_links):
+    for i in genre_links:
+        print(genre_links(i))
+
+
+def get_user_genre_choice(start_choice, last_choice):
+    return input(f'Choose genre ({start_choice}-{last_choice} ): ')
+    # looking up https://docs.python.org/3/tutorial/inputoutput.html
+
+
+def get_genre_count(links):
+    return len(links)
+
+def load_genre(web_driver, user_choice):
+    movies = web_driver.find_elements(By.XPATH, "//a[@href]")
+    movie_links = []
+    for movie in movies:
+        link = movie.get_attribute('href')
+        if 'genre' in movie.get_attribute('href'):
+            return movie_links
+
+
+# you shouldn't just delete all of the notes that help you remember what shit does
+# instead you should rewrite or replace the comments with more summarized versions
+# just keep it simple
+
+# yeah this shit is starting to get confusing
+
+def parse_movies(movie_links):
+    print("ok")
 
 
 def main():
-        if internet_working():
-                driver = SetupFirefox()
-                links = GetGenres(driver)
-                write_to_file(GetUrlSource(URL, driver))
-                try:
-                        driver.close()
-                except:
-                        pass
+    display_message()
+    if internet_working():
+        chrome = setup_chrome()
+        links = get_genres(chrome)
 
-                print(links)
+        display_genres_to_user(links)
 
-main()
+        # asking the user from dynamically counting the links
+        choice = get_user_genre_choice(1, get_genre_count(links))
+        # starting to user the driver to display list of movies from that ge
+        load_genre(chrome, choice)
 
-#testing links change this later
-def setupChrome():
-        opts = ChromeOptions()
-        driver2 = webdriver.Chrome(options=opts)
-        Join = input('''Choose genre: ''')
-        if Join.lower() == 'kids':
-          driver2.get("https://vipstream.tv/genre/kids")
-        elif Join.lower() == 'horror':
-          driver2.get('https://vipstream.tv/genre/horror')
-        elif Join.lower() == 'soap':
-          driver2.get('https://vipstream.tv/genre/soap')
-        else:
-          print("Wrong Choice. Try Again")
+        try:
+            chrome.close()
+        except:
+            pass
 
-setupChrome()
+# this is only test code leave here
 
 
+# case switch in python google
+# also instead of typing the genre name you can just make the user choose a number
+# starting from 0 - max_choice
+
+# stop saying sorry and saying you're a noob
+# just focus on what's happening with the code
+
+# TODO: work on making chrome driver HEADLESS AGAIN!!!
 
 
+# we need that datatype with all the links in the one array
+# [link1,link2, link3]
+# we use this datastructure then we just
+#  check if choice is valid integer
+# then webdriver.get(urls[choice])
+# so how do we get the urls from the [link1, link2, link3]
+# im just confused how we get the urls from the array
+# choose any number from the array by taking the name of the array arrayName[number of item you want from array (0-arrayName(length))]
 
-
+# so we get the integer input? like
+# just normal user input
+# check if its (1-28)
+# if user didn't write 1-28 then show an error and tell the user to choose number between 1-28
+# TODO: create a choice loop (retry function)
+# worry about it LATER
+# but make a note of what you want to do
 
 
