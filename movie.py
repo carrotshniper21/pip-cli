24ef4129dc8bbed509e6ec22218dc4f116143c3e
carrot <96284645+carrotshniper21@users.noreply.github.com>
2022-12-12 00:24:06 -0600

    Update movie.py

--- movie.py
+++ movie.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 import re
+import emulation
+import util
 
 
 def get_genres(web_driver):
@@ -39,15 +41,8 @@ def parse_movies(movie_links):
 	for movie in movie_links:
 		print(movie)
 
-
-def get_url_source(url, browser_headless):
-	browser_headless.get(url)
-	page_source = browser_headless.page_source
-	return page_source
-
-
 def get_movie_src(genre_links, user_choice, web_driver):
-	return get_url_source(genre_links[user_choice - 1], web_driver)
+	return emulation.get_url_source(genre_links[user_choice - 1], web_driver)
 
 
 def get_movies_from_genre(movie_src):
@@ -58,3 +53,87 @@ def get_movies_from_genre(movie_src):
 	movies = list(filter(regex2.search, movies))
 	movies = list(dict.fromkeys(movies))
 	return movies
+
+
+def user_choice_validator_loop(user_choice, genre_links, web_driver):
+    # loop until user enters valid choice
+    while not validate_user_genre_choice(user_choice, genre_links):
+        print("Invalid choice, try again")
+        user_choice = get_user_genre_choice(1, movie.get_genre_count(genre_links))
+    user_choice = util.convert_to_int(user_choice)
+
+    movie_src = get_movie_src(genre_links, user_choice, web_driver)
+    return get_movies_from_genre(movie_src), movie_src
+
+
+def scrape_movie_titles(movie_src):
+    # get movie titles
+    regex3 = re.compile(r'<h2 class="film-name">.+?</h2>')
+    titles = regex3.findall(movie_src)
+    titles = [title.replace('<h2 class="film-name">', '').replace('</h2>', '') for title in titles]
+    return titles
+
+def get_user_choice_movie(titles):
+    # get user choice
+    user_movie_choice = input(f"\nChoose movie(1-{len(titles)}): ")
+    return util.convert_to_int(user_movie_choice)
+
+def display_titles(titles):
+    for i, title in enumerate(titles):
+        print(i + 1, title)
+
+def get_movie_server_urls(movie_page_src, prefix):
+    # get movie servers and links
+    regex5 = re.compile(r'href="(.+?)"')
+    regex6 = re.compile(r'/watch-movie/.+?')
+    movie_servers = regex5.findall(movie_page_src)
+    movie_servers = list(filter(regex6.search, movie_servers))
+    movie_servers = list(dict.fromkeys(movie_servers))
+    movie_servers = [prefix + link for link in movie_servers]
+    movie_servers.pop(0)
+    return movie_servers
+
+def get_movie_server_names(movie_page_src):
+    # get server names from span
+    regex7 = re.compile(r'<span>.+?</span>')
+    server_names = regex7.findall(movie_page_src)
+
+    # refine server names ( there is only 3)
+    server_names = [name.replace('<span>', '').replace('</span>', '') for name in server_names]
+    server_names = server_names[2:5]
+    print(server_names)
+    return server_names
+
+def get_user_server_choice(server_names):
+    # get user choice
+    user_server_choice = input(f"\nChoose server(1-{len(server_names)}): ")
+    return util.convert_to_int(user_server_choice)
+
+def loading_message():
+    print("Loading Selections...\n")
+
+
+def init_movie_protocol(prefix):
+    # choose browser firefox or chrome
+    print("Choose browser:\n1. Firefox\n2. Chrome")
+    browser_choice = input("Enter choice: ")
+    # if browser choice is firefox
+    web_driver = emulation.validate_browser_choice(browser_choice)
+    loading_message()
+
+    genre_links = get_genres(web_driver)
+    movie.display_genres_to_user(genre_links)
+
+    user_choice = get_user_genre_choice(1, get_genre_count(genre_links))
+    print('\n')
+
+    movies, movie_src = user_choice_validator_loop(user_choice, genre_links, web_driver)
+    titles = scrape_movie_titles(movie_src)
+    user_movie_choice = get_user_choice_movie(titles)
+    movie_url = prefix + movies[user_movie_choice - 1]
+    movie_page_src = emulation.get_url_source(movie_url, web_driver)
+    # movie_servers = get_movie_server_urls(movie_page_src, prefix)
+    server_names = get_movie_server_names(movie_page_src)
+
+    # close the driver on the user
+    emulation.web_driver_soft_close(web_driver)
