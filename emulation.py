3455b1e5afb01314ad5193f89606e79416a407b0
carrot <96284645+carrotshniper21@users.noreply.github.com>
2022-12-12 00:25:20 -0600

    Update emulation.py

--- emulation.py
+++ emulation.py
@@ -1,6 +1,16 @@
-from selenium import webdriver
+rom selenium import webdriver
 from selenium.webdriver import ChromeOptions
 import config
+
+def validate_browser_choice(browser_choice):
+    if browser_choice == "1" or browser_choice == "firefox":
+        return setup_firefox()
+        # if browser choice is chrome
+    elif input == "2" or browser_choice == "chrome":
+        return setup_chrome()
+    else:
+        print("Invalid choice, defaulting to Firefox")
+        return setup_firefox()
 
 
 def setup_firefox():
@@ -9,7 +19,6 @@ def setup_firefox():
     driver_f = webdriver.Firefox(options=options)
     driver_f.get(config.BASE_URL)
     return driver_f
-
 
 def setup_chrome():
     opts = ChromeOptions()
@@ -23,3 +32,8 @@ def web_driver_soft_close(web_driver):
         web_driver.close()
     except:
         pass
+
+def get_url_source(url, browser_headless):
+    browser_headless.get(url)
+    page_source = browser_headless.page_source
+    return page_source
