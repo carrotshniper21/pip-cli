70444b59becbf34b2248218f5a4e0ae1d2d44b91
carrot <96284645+carrotshniper21@users.noreply.github.com>
2022-12-12 00:31:31 -0600

    Update help.sh

--- help.sh
+++ help.sh
@@ -1,37 +1,51 @@
-#!/bin/bash
+#!/usr/bin/env python3
 
-display_help() {
-    echo '''Usage: [option...] [-h] [-d]
+import getopt
+import sys
+import subprocess
+
+argumentList = sys.argv[1:]
+
+options = "hd"
+
+long_options = ["help", "download"]
+
+def usage():
+    print('''Usage: [option...] [-h] [-d]
             -h, --help              Display this message and exit
-            -d, --download          Download the selected media'''
+            -d, --download          Download the selected media''')
 
-    exit 1
-}
 
-while :
-do
-    case "$1" in
-      -h | --help)
-          display_help
-          exit 0
-          ;;
+def main():
+    try:
+        arguments, values = getopt.getopt(argumentList, options, long_options)
 
-      -d | --download)
-          echo "Downloading..."
-          shift
-          ;;
+        for currentArgument, currentValue in arguments:
+            if currentArgument in ("-h", "--help"):
+                usage()
+            elif currentArgument in ("-d", "--download"):
+                print ("Displaying file_name:", sys.argv[0])
 
-      --) # End of all options
-          shift
-          break
-          ;;
-      -*)
-          echo "Error: Unknown option: $1" >&2
-          exit 1
-          ;;
-      *)  # No more options
-          break
-          ;;
-    esac
-done
+    except getopt.error as err:
+        print (str(err))
 
+#TODO fix the checker
+
+    # check if user has pip installed
+    # if not subprocess.call("pip --version", shell=True):
+        # print("pip is not installed")
+        # exit(1)
+
+    # check if user has virtualenv installed
+    # if not subprocess.call("virtualenv --version", shell=True):
+        # print("virtualenv is not installed")
+        # exit(1)
+
+    # check if user has python3 installed
+    # if not subprocess.call("python3 --version", shell=True):
+        # print("python3 is not installed")
+        # exit(1)
+
+
+if __name__ == "__main__":
+    main()
