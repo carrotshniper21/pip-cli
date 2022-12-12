f904ea9a0a30710c8fc861e2beed5a079d7630c6
carrot <96284645+carrotshniper21@users.noreply.github.com>
2022-12-12 00:40:31 -0600

    Update chromedriver_fetch.py

--- chromedriver_fetch.py
+++ chromedriver_fetch.py
@@ -25,3 +25,22 @@ def define_download(version_number):
     os.remove(latest_driver_zip)
 
 define_download(version_number)
+
+#import requests
+#import wget
+#import zipfile
+#import os
+#import sys
+
+#url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+#response = requests.get(url)
+#version_number = response.text
+
+#def define_download(version_number):
+#    download_url = "https://chromedriver.storage.googleapis.com/{}/chromedriver_{}64.zip".format(version_number, sys.platform)
+#    latest_driver_zip = wget.download(download_url,'chromedriver.zip')
+
+#    zipfile.ZipFile(latest_driver_zip).extractall()
+#    os.remove(latest_driver_zip)
+
+#define_download(version_number)
